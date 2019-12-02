4d8d4abb8dd63df83a1aa986cce19a84cf67bedc
kyechou <kuanyenchou@gmail.com>
2019-12-02 14:08:35 -0600

    Found calling address (block and capstone insn)

--- test-angr.py
+++ test-angr.py
@@ -2,22 +2,6 @@
 
 import angr
 
-visited_blocks = set()
-
-def DFS_search(function, block):
-    # TODO: search for the accept() call in block
-    # if found, return the address and block
-    if block in visited_blocks:
-        return None
-    visited_blocks.add(block)
-    print(block)
-
-    for successor in function.graph.successors(block):
-        found = DFS_search(function, successor)
-        if found:
-            return found
-    return None
-
 # def main():
 TARGET='lb'
 
@@ -29,25 +13,37 @@ cg = cfg.functions.callgraph
 #sm = p.factory.simulation_manager(state)
 #sm.run(until=lambda sm_: len(sm_.active) > 1)
 
-accepts = list()
-
-for _, func in cfg.kb.functions.items():
-    if func.name == 'accept':
-        accepts.append(func)
-
+accept = cfg.kb.functions['accept']
 parents = list()
 
-for accept in accepts:
-    for parent_addr in cg.predecessors(accept.addr):
-        parent_func = cfg.kb.functions[parent_addr]
-        if parent_func.name != 'accept':
-            parents.append(parent_func)
+for parent_addr in cg.predecessors(accept.addr):
+    parent_func = cfg.kb.functions[parent_addr]
+    if parent_func.name != 'accept':
+        parents.append(parent_func)
 
 # assuming there's only one piece of logic making the decision, which should
 # be true under common circumstances
 parent = parents[0]
-visited_blocks.clear()
-DFS_search(parent, parent.startpoint)
+visited_nodes = set()
+
+def DFS_search(project, function, node):
+    # search for the accept() call in node (BlockNode)
+    # if found, return the block and the instruction
+    if node in visited_nodes:
+        return None
+    visited_nodes.add(node)
+    block = project.factory.block(node.addr)
+    for insn in block.capstone.insns:
+        if insn.insn.insn_name() == 'call' and insn.op_str == hex(accept.addr):
+            return (block, insn)
+
+    for successor in function.graph.successors(node):
+        found = DFS_search(project, function, successor)
+        if found:
+            return found
+    return None
+
+block, capstone_insn = DFS_search(p, parent, parent.startpoint)
 
 #if __name__ == '__main__':
 #    main()
