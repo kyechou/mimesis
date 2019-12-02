731fa32c84b784b946b0482c34554a33f1ac9624
kyechou <kuanyenchou@gmail.com>
2019-12-02 12:32:12 -0600

    DFS through the in-function CFG

--- test-angr.py
+++ test-angr.py
@@ -2,6 +2,23 @@
 
 import angr
 
+visited_blocks = set()
+
+def DFS_search(function, block):
+    # TODO: search for the accept() call in block
+    # if found, return the address and block
+    if block in visited_blocks:
+        return None
+    visited_blocks.add(block)
+    print(block)
+
+    for successor in function.graph.successors(block):
+        found = DFS_search(function, successor)
+        if found:
+            return found
+    return None
+
+# def main():
 TARGET='lb'
 
 p = angr.Project(TARGET, auto_load_libs=False)
@@ -18,16 +35,6 @@ for _, func in cfg.kb.functions.items():
     if func.name == 'accept':
         accepts.append(func)
 
-#for block in main.blocks:
-#    print("=======================")
-#    block.pp()
-
-#for s in cg.successors(main.addr):
-#for s in cg.successors(p.entry):
-#    succ_func = cfg.kb.functions[s]
-#    if succ_func.name == 'accept':
-#        pass
-
 parents = list()
 
 for accept in accepts:
@@ -36,8 +43,11 @@ for accept in accepts:
         if parent_func.name != 'accept':
             parents.append(parent_func)
 
-# assuming there's only one piece of logic making the decision
+# assuming there's only one piece of logic making the decision, which should
+# be true under common circumstances
 parent = parents[0]
+visited_blocks.clear()
+DFS_search(parent, parent.startpoint)
 
-for block in parent.blocks:
-    print(block)
+#if __name__ == '__main__':
+#    main()
