ff1dba82a3ec200208ce853f6b069bd20a4e0ca8
kyechou <kuanyenchou@gmail.com>
2024-02-21 09:39:59 -0600

    Add hello world 1

---
+++ targets/hello-world/hw1.cpp
@@ -0,0 +1,11 @@
+/**
+ * Hello, world 1
+ */
+
+#include <iostream>
+
+int main() {
+    std::cout << "[stdout] Hello, world!" << std::endl;
+    std::cerr << "[stderr] Hello, world!" << std::endl;
+    return 0;
+}
