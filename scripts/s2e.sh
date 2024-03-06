8bc62c8210a1fb1100bae90cb583d1381f984823
kyechou <kuanyenchou@gmail.com>
2024-03-06 09:31:36 -0600

    Allow file system sharing between QEMU host and guest through 9P_FS

--- scripts/s2e.sh
+++ scripts/s2e.sh
@@ -77,64 +77,6 @@ parse_args() {
     fi
 }
 
-# Create a QEMU snapshot to reduce VM startup time.
-create_qemu_snapshot() {
-    if [[ ! -e "$NUM_INTFS_FILE" ]]; then
-        die "File not found: $NUM_INTFS_FILE"
-    fi
-
-    local interfaces
-    interfaces=$(<"$NUM_INTFS_FILE")
-    local qemu_flags=()
-    for ((i = 1; i <= interfaces; ++i)); do
-        qemu_flags+=("-nic tap,ifname=tap$i,script=no,downscript=no,model=e1000")
-    done
-
-    local s2e_image_dir="$S2E_DIR/images/ubuntu-22.04-x86_64"
-    local s2e_image="$s2e_image_dir/image.raw.s2e"
-    local snapshot_intfs_file="$s2e_image_dir/snapshot_interfaces.txt"
-    local image='kyechou/s2e-builder:latest'
-    local snapshot_cmd
-
-    # No need to create a snapshot if the number of interfaces is the same.
-    if [[ -e "$snapshot_intfs_file" ]]; then
-        local snapshot_intfs
-        snapshot_intfs=$(<"$snapshot_intfs_file")
-        if [[ $interfaces -eq $snapshot_intfs ]]; then
-            return 0
-        fi
-    fi
-
-    snapshot_cmd="$(
-        cat <<-EOM
-        set -euo pipefail
-        for i in {1..$interfaces}; do
-            sudo ip tuntap add mode tap tap\$i
-        done
-
-        LD_PRELOAD=$S2E_INSTALL_DIR/share/libs2e/libs2e-x86_64.so \
-            $S2E_INSTALL_DIR/bin/qemu-system-x86_64 \
-            -enable-kvm -m 256M -nographic -monitor null \
-            -drive file=$s2e_image,format=s2e,cache=writeback \
-            -serial file:$s2e_image_dir/serial_ready.txt \
-            -enable-serial-commands \
-            ${qemu_flags[@]}
-
-        for i in {1..$interfaces}; do
-            sudo ip tuntap del mode tap tap\$i
-        done
-EOM
-    )"
-    docker run -it --rm --privileged -u builder \
-        -v "$PROJECT_DIR:$PROJECT_DIR" \
-        "$image" \
-        -c "$snapshot_cmd"
-
-    # Set the number of interfaces for the snapshot.
-    echo "$interfaces" >"$snapshot_intfs_file"
-    chmod 400 "$snapshot_intfs_file"
-}
-
 new_project() {
     # Remove the s2e project directory if it exists
     if [[ -e "$S2E_PROJ_DIR" ]]; then
@@ -181,12 +123,17 @@ EOM
     # 2. Enable privileges for the target program. (Alternative: setuid)
     # 3. Load systemtap kernel modules before the target program.
     # 4. Turn on the interfaces before the target program.
+    # 5. Mount the 9P_FS virtio file system.
     local capabilities='cap_sys_admin+pe cap_net_admin+pe cap_net_raw+pe cap_sys_ptrace+pe'
     local if_cmds="ip link | grep '^[0-9]\\\\+' | cut -d: -f2 | sed 's/ //g' | grep -v '^lo' | grep -v '^sit' | xargs -I{} sudo ip link set {} up\n"
+    local guest_share_dir='/dev/shm/mimesis'
+    local mount_cmds=
+    mount_cmds+="mkdir -p $guest_share_dir\n"
+    mount_cmds+="sudo mount -t 9p -o trans=virtio -o version=9p2000.L host0 $guest_share_dir\n"
     sed -i "$S2E_PROJ_DIR/bootstrap.sh" \
         -e 's,\(> */dev/null \+2> */dev/null\),# \1,' \
         -e "s,^\( *S2E_SYM_ARGS=\".*\"\),    sudo setcap \"$capabilities\" \"\${TARGET}\"\n\1," \
-        -e "s,^\(execute \"\${TARGET_PATH}\"\),${systemtap_cmds}${if_cmds}\1,"
+        -e "s,^\(execute \"\${TARGET_PATH}\"\),${systemtap_cmds}${if_cmds}${mount_cmds}\1,"
 
     # 1. Enable the custom plugin for Mimesis.
     # 2. Disable unused Lua plugins.
@@ -220,9 +167,68 @@ EOM
 
     # Set the number of interfaces
     echo "$INTERFACES" >"$NUM_INTFS_FILE"
-    chmod 400 "$NUM_INTFS_FILE"
+    chmod 600 "$NUM_INTFS_FILE"
     # Create a QEMU snapshot to reduce VM startup time.
     create_qemu_snapshot
+}
+
+# Create a QEMU snapshot to reduce VM startup time.
+create_qemu_snapshot() {
+    if [[ ! -e "$NUM_INTFS_FILE" ]]; then
+        die "File not found: $NUM_INTFS_FILE"
+    fi
+
+    local interfaces
+    interfaces=$(<"$NUM_INTFS_FILE")
+    local qemu_flags=()
+    for ((i = 1; i <= interfaces; ++i)); do
+        qemu_flags+=("-nic tap,ifname=tap$i,script=no,downscript=no,model=e1000")
+    done
+
+    local s2e_image_dir="$S2E_DIR/images/ubuntu-22.04-x86_64"
+    local s2e_image="$s2e_image_dir/image.raw.s2e"
+    local snapshot_intfs_file="$s2e_image_dir/snapshot_interfaces.txt"
+    local image='kyechou/s2e-builder:latest'
+    local snapshot_cmd
+
+    # No need to create a snapshot if the number of interfaces is the same.
+    if [[ -e "$snapshot_intfs_file" ]]; then
+        local snapshot_intfs
+        snapshot_intfs=$(<"$snapshot_intfs_file")
+        if [[ $interfaces -eq $snapshot_intfs ]]; then
+            return 0
+        fi
+    fi
+
+    snapshot_cmd="$(
+        cat <<-EOM
+        set -euo pipefail
+        for i in {1..$interfaces}; do
+            sudo ip tuntap add mode tap tap\$i
+        done
+
+        mkdir $HOST_SHARE_DIR
+        LD_PRELOAD=$S2E_INSTALL_DIR/share/libs2e/libs2e-x86_64.so \
+            $S2E_INSTALL_DIR/bin/qemu-system-x86_64 \
+            -enable-kvm -m 256M -nographic -monitor null \
+            -drive file=$s2e_image,format=s2e,cache=writeback \
+            -serial file:$s2e_image_dir/serial_ready.txt \
+            -enable-serial-commands \
+            ${qemu_flags[@]}
+
+        for i in {1..$interfaces}; do
+            sudo ip tuntap del mode tap tap\$i
+        done
+EOM
+    )"
+    docker run -it --rm --privileged -u builder \
+        -v "$PROJECT_DIR:$PROJECT_DIR" \
+        "$image" \
+        -c "$snapshot_cmd"
+
+    # Set the number of interfaces for the snapshot.
+    echo "$interfaces" >"$snapshot_intfs_file"
+    chmod 600 "$snapshot_intfs_file"
 }
 
 run_s2e() {
@@ -233,6 +239,7 @@ run_s2e() {
     for ((i = 1; i <= interfaces; ++i)); do
         qemu_flags+=("-nic tap,ifname=tap$i,script=no,downscript=no,model=e1000")
     done
+    qemu_flags+=("-virtfs local,path=$HOST_SHARE_DIR,mount_tag=host0,security_model=passthrough,id=host0")
     local run_cmd
     run_cmd="$(
         cat <<-EOM
@@ -242,7 +249,9 @@ run_s2e() {
             sudo ip tuntap add mode tap tap\$i
         done
 
+        mkdir $HOST_SHARE_DIR
         ./launch-s2e.sh ${qemu_flags[@]}
+        mv $HOST_SHARE_DIR/* $S2E_PROJ_DIR/
 
         for i in {1..$interfaces}; do
             sudo ip tuntap del mode tap tap\$i
@@ -269,6 +278,7 @@ main() {
     S2E_PROJ_DIR="$S2E_DIR/projects/$S2E_PROJ_NAME"
     S2E_INSTALL_DIR="$S2E_DIR/install"
     NUM_INTFS_FILE="$S2E_PROJ_DIR/num_interfaces.txt"
+    HOST_SHARE_DIR='/dev/shm/mimesis'
 
     if [[ $RM -eq 1 ]]; then
         rm -rf "$PROJECT_DIR/s2e/s2e/projects"/*
