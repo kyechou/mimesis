b1f8cca7b226a12e4e8fe0d23e66f687614a7bd2
kyechou <kuanyenchou@gmail.com>
2024-03-07 15:42:05 -0600

    Repeatedly send packets every 1 second

--- src/sender.cpp
+++ src/sender.cpp
@@ -1,13 +1,14 @@
 #include "sender.hpp"
 
+#include <algorithm>
 #include <cerrno>
 #include <chrono>
+#include <condition_variable>
 #include <cstring>
 #include <filesystem>
 #include <fstream>
-#include <sstream>
+#include <mutex>
 #include <string>
-#include <system_error>
 #include <unistd.h>
 #include <vector>
 
@@ -21,58 +22,88 @@ using namespace std;
 namespace fs = std::filesystem;
 
 int main() {
-    Packet packet;
-    memset(&packet, 0, sizeof(packet));
-    vector<Interface> interfaces = open_existing_interfaces(/*tap_only=*/true);
+    // Variables for synchronization between threads.
+    string dst_if_name;    // the interface to which packets are sent
+    mutex mtx;             // lock for dst_if_name
+    condition_variable cv; // for reading dst_if_name
 
-    if (interfaces.empty()) {
-        error("No interfaces available");
-    }
+    // Packet sender.
+    auto packet_sender = [&dst_if_name, &mtx,
+                          &cv](chrono::milliseconds period) {
+        unique_lock<mutex> lck(mtx);
+        Packet packet;
+        memset(&packet, 0, sizeof(packet));
+        vector<Interface> interfaces =
+            open_existing_interfaces(/*tap_only=*/true);
 
-    info("Starting the sender with " + to_string(interfaces.size()) +
-         " interfaces");
+        if (interfaces.empty()) {
+            error("No interfaces available");
+        }
 
-    auto notification_handler = [&](inotify::Notification notification) {
-        auto ts = chrono::duration_cast<chrono::microseconds>(
-                      notification.time.time_since_epoch())
-                      .count();
-        const auto &intf = interfaces.at(0);
-        stringstream ss;
-        ss << "[" << ts << "] Sending a packet to " << intf.if_name;
-        info(ss.str());
+        info("Found " + to_string(interfaces.size()) + " interfaces");
 
-        ssize_t nwrite = write(intf.fd, &packet, sizeof(packet));
-        if (nwrite < 0) {
-            error("Failed to send packet", errno);
+        while (1) {
+            cv.wait_for(lck, period);
+            if (dst_if_name.empty()) {
+                continue;
+            }
+
+            // Find the interface file descriptor.
+            auto if_it = find_if(interfaces.begin(), interfaces.end(),
+                                 [&dst_if_name](const Interface &i) {
+                                     return i.if_name == dst_if_name;
+                                 });
+            if (if_it == interfaces.end()) {
+                warn("Interface not found: " + dst_if_name);
+                continue;
+            }
+
+            info("Sending a packet to " + dst_if_name);
+            ssize_t nwrite = write(if_it->fd, &packet, sizeof(packet));
+            if (nwrite < 0) {
+                error("Failed to send packet", errno);
+            }
         }
     };
+    std::thread sending_thread(packet_sender, chrono::milliseconds(1000));
 
-    // Create the send_packet file at the shared folder.
-    fs::path shared_dir("/dev/shm/mimesis");
-    fs::path send_packet_file(shared_dir / "send_packet");
-    error_code err;
-    if (!fs::create_directory(shared_dir, err) && err) {
-        error("Failed to create directory " + shared_dir.string() + ": " +
-              err.message());
-    }
-    ofstream send_packet(send_packet_file);
+    // Create the send_packet file.
+    const fs::path send_packet_fn("/dev/shm/send_packet");
+    ofstream send_packet(send_packet_fn);
     if (!send_packet) {
-        error("Failed to open " + send_packet_file.string());
+        error("Failed to open " + send_packet_fn.string());
     }
     send_packet.close();
+
+    // File notification handler.
+    auto notification_handler = [&dst_if_name, &mtx,
+                                 &cv](inotify::Notification notification) {
+        string if_name;
+        ifstream send_packet_file(notification.path);
+        send_packet_file >> if_name;
+        send_packet_file.close();
+        info("Notification handler read '" + if_name + "' interface name");
+
+        lock_guard<mutex> lck(mtx);
+        dst_if_name = if_name;
+        cv.notify_all();
+    };
 
     // Monitor the send_packet file on modification.
     auto events = {
         inotify::Event::modify, // File was modified.
     };
     auto notifier = inotify::BuildNotifier()
-                        .watchFile(send_packet_file)
+                        .watchFile(send_packet_fn)
                         .onEvents(events, notification_handler);
-    info("Start monitoring " + send_packet_file.string());
-    std::thread event_loop_thread([&notifier]() { notifier.run(); });
+    info("Monitoring " + send_packet_fn.string());
+    std::thread monitor_thread([&notifier]() { notifier.run(); });
 
-    if (event_loop_thread.joinable()) {
-        event_loop_thread.join();
+    if (sending_thread.joinable()) {
+        sending_thread.join();
+    }
+    if (monitor_thread.joinable()) {
+        monitor_thread.join();
     }
 
     info("Exiting");
