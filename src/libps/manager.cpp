0126c0aa601f79c7afc1a9e96c2324d7719210b6
kyechou <kuanyenchou@gmail.com>
2024-04-07 03:52:09 -0500

    Update libps manager for init and config

--- src/libps/manager.cpp
+++ src/libps/manager.cpp
@@ -1,6 +1,7 @@
 #include "libps/manager.hpp"
 
 #include <cerrno>
+#include <cstdint>
 #include <cstdio>
 #include <sylvan.h>
 #include <sylvan_common.h>
@@ -8,112 +9,70 @@
 
 #include "lib/logger.hpp"
 
-// `third_party/sylvan/sylvan/examples/simple.cpp`:
-//
-// VOID_TASK_0(simple_cxx)
-// {
-//     Bdd one = Bdd::bddOne(); // the True terminal
-//     Bdd zero = Bdd::bddZero(); // the False terminal
-//
-//     // check if they really are the True/False terminal
-//     assert(one.GetBDD() == sylvan_true);
-//     assert(zero.GetBDD() == sylvan_false);
-//
-//     Bdd a = Bdd::bddVar(0); // create a BDD variable x_0
-//     Bdd b = Bdd::bddVar(1); // create a BDD variable x_1
-//
-//     // check if a really is the Boolean formula "x_0"
-//     assert(!a.isConstant());
-//     assert(a.TopVar() == 0);
-//     assert(a.Then() == one);
-//     assert(a.Else() == zero);
-//
-//     // check if b really is the Boolean formula "x_1"
-//     assert(!b.isConstant());
-//     assert(b.TopVar() == 1);
-//     assert(b.Then() == one);
-//     assert(b.Else() == zero);
-//
-//     // compute !a
-//     Bdd not_a = !a;
-//
-//     // check if !!a is really a
-//     assert((!not_a) == a);
-//
-//     // compute a * b and !(!a + !b) and check if they are equivalent
-//     Bdd a_and_b = a * b;
-//     Bdd not_not_a_or_not_b = !(!a + !b);
-//     assert(a_and_b == not_not_a_or_not_b);
-//
-//     // perform some simple quantification and check the results
-//     Bdd ex = a_and_b.ExistAbstract(a); // \exists a . a * b
-//     assert(ex == b);
-//     Bdd andabs = a.AndAbstract(b, a); // \exists a . a * b using AndAbstract
-//     assert(ex == andabs);
-//     Bdd univ = a_and_b.UnivAbstract(a); // \forall a . a * b
-//     assert(univ == zero);
-//
-//     // alternative method to get the cube "ab" using bddCube
-//     BddSet variables = a * b;
-//     std::vector<unsigned char> vec = {1, 1};
-//     assert(a_and_b == Bdd::bddCube(variables, vec));
-//
-//     // test the bddCube method for all combinations
-//     assert((!a * !b) == Bdd::bddCube(variables, std::vector<uint8_t>({0,
-//     0}))); assert((!a * b)  == Bdd::bddCube(variables,
-//     std::vector<uint8_t>({0, 1}))); assert((!a)      ==
-//     Bdd::bddCube(variables, std::vector<uint8_t>({0, 2}))); assert((a * !b)
-//     == Bdd::bddCube(variables, std::vector<uint8_t>({1, 0}))); assert((a * b)
-//     == Bdd::bddCube(variables, std::vector<uint8_t>({1, 1}))); assert((a) ==
-//     Bdd::bddCube(variables, std::vector<uint8_t>({1, 2}))); assert((!b) ==
-//     Bdd::bddCube(variables, std::vector<uint8_t>({2, 0}))); assert((b) ==
-//     Bdd::bddCube(variables, std::vector<uint8_t>({2, 1}))); assert(one ==
-//     Bdd::bddCube(variables, std::vector<uint8_t>({2, 2})));
-// }
-
-// Maybe useful functions:
-//
-// mtbdd_satcount(bdd, number_of_vars): compute the number of minterms
-// (assignments that lead to True) for a function with <number_of_vars>
-// variables; we don’t need to know the exact variables that may be in the BDD,
-// just how many there are.
-//
-// sylvan_pathcount(bdd): compute the number of distinct paths to True.
-//
-// mtbdd_nodecount(bdd): compute the number of nodes (and leaves) in the BDD.
-//
-// mtbdd_nodecount_more(array, length): compute the number of nodes (and leaves)
-// in the array of BDDs.
-
 namespace ps {
-
-Manager::Manager() {
-    // The worker threads will busy-wait until a task is offered.
-    lace_start(/*n_workers=*/1, /*dqsize=*/0);
-    sylvan::sylvan_set_limits(
-        /*memory_cap=*/1UL * 1024 * 1024 * 1024, // 1 GB (value in bytes)
-        /*table_ratio=*/1,  // Node table is twice (2^1) as big as the operation
-                            // cache.
-        /*initial_ratio=*/5 // The initial tables are 2^5 times smaller than the
-                            // max size
-    );
-    sylvan::sylvan_init_package();
-    sylvan::Sylvan::initBdd();
-    lace_suspend();
-}
-
-Manager::~Manager() {
-    lace_resume();
-    sylvan::sylvan_quit();
-    lace_stop();
-}
 
 Manager &Manager::get() {
     static Manager instance;
     return instance;
 }
 
+void Manager::init(size_t n_workers,
+                   size_t memory_cap,
+                   int table_ratio,
+                   int initial_ratio) {
+    lace_start(n_workers, /*dqsize=*/0);
+    sylvan::sylvan_set_limits(memory_cap, table_ratio, initial_ratio);
+    sylvan::sylvan_init_package();
+    sylvan::Sylvan::initBdd();
+    lace_suspend();
+    _initialized = true;
+}
+
+Manager::~Manager() {
+    if (!_initialized) {
+        warn("libps is not initialized");
+        return;
+    }
+
+    lace_resume();
+    sylvan::sylvan_quit();
+    lace_stop();
+}
+
+void Manager::register_symbolic_variable(const std::string &var_name,
+                                         uint32_t nbits) {
+    if (!_initialized) {
+        warn("libps is not initialized");
+        return;
+    }
+
+    auto res = _variables.insert({
+        var_name, {_starting_bddnode_index, nbits}
+    });
+
+    if (!res.second) {
+        error("Attempting to register symbolic variable '" + var_name +
+              "' more than once");
+    }
+
+    _starting_bddnode_index += nbits;
+}
+
+std::pair<uint32_t, uint32_t>
+Manager::get_variable_offset(const std::string &var_name) const {
+    auto it = _variables.find(var_name);
+    if (it == _variables.end()) {
+        error("Variable '" + var_name + "' not found");
+    }
+    return it->second;
+}
+
 void Manager::report_stats(FILE *out) const {
+    if (!_initialized) {
+        warn("libps is not initialized");
+        return;
+    }
+
     lace_resume();
     sylvan::sylvan_stats_report(out);
     lace_suspend();
--- src/libps/manager.hpp
+++ src/libps/manager.hpp
@@ -1,13 +1,21 @@
 #ifndef LIBPS_MANAGER_HPP
 #define LIBPS_MANAGER_HPP
 
+#include <cstdint>
 #include <cstdio>
+#include <string>
+#include <unordered_map>
+#include <utility>
 
 namespace ps {
 
 class Manager {
 private:
-    Manager();
+    bool _initialized = false;
+    uint32_t _starting_bddnode_index = 0;
+    std::unordered_map<std::string, std::pair<uint32_t, uint32_t>> _variables;
+
+    Manager() = default;
 
 public:
     // Disable the copy/move constructors and the assignment operators
@@ -19,6 +27,30 @@ public:
 
     static Manager &get();
 
+    /**
+     * See the following functions for parameter definitions.
+     * - lace_start
+     * - sylvan::sylvan_set_limits
+     */
+    void init(size_t n_workers,
+              size_t memory_cap,
+              int table_ratio,
+              int initial_ratio);
+    /**
+     * All symbolic variables must be registered before being used to create
+     * BDDs.
+     */
+    void register_symbolic_variable(const std::string &var_name,
+                                    uint32_t nbits);
+    /**
+     * Returns the bit offset information (starting bit, number of bits) of the
+     * symbolic variable `var_name`.
+     */
+    std::pair<uint32_t, uint32_t>
+    get_variable_offset(const std::string &var_name) const;
+    /**
+     * Write sylvan stats report to `out`.
+     */
     void report_stats(FILE *out) const;
 };
 
