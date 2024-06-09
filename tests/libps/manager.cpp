5de7ec7b8ee9d20857cd17979d37a3d3fabde5bf
kyechou <kuanyenchou@gmail.com>
2024-06-08 22:26:55 -0500

    Clean up tests for packetset and manager

---
+++ tests/libps/manager.cpp
@@ -0,0 +1,30 @@
+#include <cstdint>
+#include <gtest/gtest.h>
+#include <linux/if_ether.h>
+#include <string>
+
+#include "libps/manager.hpp"
+
+struct DemoHeader {
+    uint16_t seed; // egress port
+    uint16_t len;  // payload length
+};
+
+class ManagerTests : public testing::Test {
+protected:
+    const std::string var_name = "ingress_packet";
+
+    void SetUp() override {
+        // Initialize libps
+        ps::Manager::get().init(/*n_workers=*/1,
+                                /*memory_cap=*/1UL * 1024 * 1024 * 1024,
+                                /*table_ratio=*/1,
+                                /*initial_ratio=*/5);
+
+        // Register symbolic variable
+        auto hdr_len = sizeof(struct ethhdr) + sizeof(struct DemoHeader);
+        ps::Manager::get().register_symbolic_variable(var_name, hdr_len * 8);
+    }
+
+    void TearDown() override { ps::Manager::get().reset(); }
+};
--- tests/libps/packetset.cpp
+++ tests/libps/packetset.cpp
@@ -2,14 +2,13 @@
 #include <gtest/gtest.h>
 #include <klee/Common.h>
 #include <klee/Expr.h>
+#include <klee/util/Ref.h>
 #include <linux/if_ether.h>
 #include <llvm/Support/raw_ostream.h>
 #include <string>
 #include <unistd.h>
-#include <vector>
 
 #include "libps/manager.hpp"
-#include "libps/packetset.hpp"
 
 struct DemoHeader {
     uint16_t seed; // egress port
@@ -36,89 +35,20 @@ protected:
                                 /*table_ratio=*/1,
                                 /*initial_ratio=*/5);
 
-        // Symbolic variables
-        create_symbolic_variable();
-    }
-
-    void TearDown() override {
-        delete out;
-        out = nullptr;
-        klee::klee_message_stream = nullptr;
-        klee::klee_warning_stream = nullptr;
-        ps::Manager::get().reset();
-    }
-
-    void create_symbolic_variable() {
+        // Create symbolic variable
         auto hdr_len = sizeof(struct ethhdr) + sizeof(struct DemoHeader);
         array = klee::Array::create(var_name, hdr_len);
         ul = klee::UpdateList::create(array, 0);
         ps::Manager::get().register_symbolic_variable(var_name, hdr_len * 8);
     }
 
-    /**
-     * This function tries to recreate the first condition expression we
-     * encountered in demo-r1.
-     */
-    klee::ref<klee::Expr> create_expr_1() {
-        // (Ule
-        //  (ZExt w64
-        //      (Extract w16 0
-        //          (And w64
-        //              (Or w64
-        //                  (LShr w64
-        //                      N0:(ZExt w64
-        //                          (Extract w16 0
-        //                              (Extract w32 0
-        //                                  (ZExt w64
-        //                                      (ReadLSB w48 0xc
-        //                                          ingress_packet)))))
-        //                      0x8)
-        //                  (Shl w64 N0 0x8))
-        //              0xffff)))
-        //  0x5ff)
-        klee::ref<klee::Expr> expr, n0;
-        std::vector<klee::ref<klee::Expr>> read_exprs;
-        for (int i = 0x11; i >= 0xc; --i) { // Read 6 bytes
-            auto e = klee::ReadExpr::create(
-                ul, klee::ConstantExpr::create(i, klee::Expr::Int32));
-            read_exprs.push_back(e);
-        }
-        expr = klee::ConcatExpr::createN(read_exprs.size(), read_exprs.data());
-        expr = klee::ZExtExpr::create(expr, klee::Expr::Int64);
-        expr = klee::ExtractExpr::create(expr, 0, klee::Expr::Int32);
-        expr = klee::ExtractExpr::create(expr, 0, klee::Expr::Int16);
-        n0 = klee::ZExtExpr::create(expr, klee::Expr::Int64);
-        expr = klee::LShrExpr::create(
-            n0, klee::ConstantExpr::create(0x8, klee::Expr::Int64));
-        expr = klee::OrExpr::create(
-            expr, klee::ShlExpr::create(
-                      n0, klee::ConstantExpr::create(0x8, klee::Expr::Int64)));
-        expr = klee::AndExpr::create(
-            expr, klee::ConstantExpr::create(0xffff, klee::Expr::Int64));
-        expr = klee::ExtractExpr::create(expr, 0, klee::Expr::Int16);
-        expr = klee::ZExtExpr::create(expr, klee::Expr::Int64);
-        expr = klee::UleExpr::create(
-            expr, klee::ConstantExpr::create(0x5ff, klee::Expr::Int64));
-        return expr;
+    void TearDown() override {
+        // Output streams
+        delete out;
+        out = nullptr;
+        klee::klee_message_stream = nullptr;
+        klee::klee_warning_stream = nullptr;
+        // libps
+        ps::Manager::get().reset();
     }
 };
-
-TEST_F(PacketSetTests, ctor) {
-    auto expr = create_expr_1();
-    auto not_expr = klee::Expr::createIsZero(expr);
-    ps::PacketSet ps(expr);
-    ps::PacketSet not_ps(not_expr);
-    EXPECT_EQ(ps, ~not_ps);
-    EXPECT_EQ(~ps, not_ps);
-    EXPECT_EQ(ps.to_string(), "[\n"
-                              "  node(1,143,0,~0),\n"
-                              "  node(2,142,1,~0),\n"
-                              "  node(3,141,2,~0),\n"
-                              "  node(4,140,3,~0),\n"
-                              "  node(5,139,4,~0),\n"
-                              "  node(6,138,5,~0),\n"
-                              "  node(7,137,5,6),\n"
-                              "],[~7,]");
-    EXPECT_EQ(ps.num_paths(), 2);
-    EXPECT_EQ(ps.size(), 9223372036854775808UL);
-}
