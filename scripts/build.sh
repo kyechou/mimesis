7cb793c04c7ca5472bedc0fcf280dbef71ce39cf
kyechou <kuanyenchou@gmail.com>
2024-08-08 23:30:03 -0500

    Disable apparmor LSM in the s2e linux kernel

--- scripts/build.sh
+++ scripts/build.sh
@@ -223,6 +223,10 @@ EOM
         -i "$PATCH_DIR/05-s2e-linux-kernel-enable-uprobes.patch")" ||
         echo "$out" | grep -q 'Skipping patch' ||
         die "$out"
+    out="$(patch -d "$S2E_DIR/source/s2e-linux-kernel" -Np1 \
+        -i "$PATCH_DIR/06-s2e-linux-kernel-disable-apparmor.patch")" ||
+        echo "$out" | grep -q 'Skipping patch' ||
+        die "$out"
 
     # Change the maximum number of interfaces allowed in QEMU.
     sed -i "$S2E_DIR/source/qemu/include/net/net.h" \
