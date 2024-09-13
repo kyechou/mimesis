52437559edfb9dfa227fb053f8f39ebd5cd5fbf5
al0wner <sjeon12@illinois.edu>
2024-09-12 23:51:28 -0500

    bug fix

--- targets/ebpf/xdp-redirect.bpf.c
+++ targets/ebpf/xdp-redirect.bpf.c
@@ -1,16 +1,25 @@
-#include "vmlinux.h"
-
-#include <bpf/bpf_core_read.h>
+#include <linux/bpf.h>
 #include <bpf/bpf_helpers.h>
+#include <linux/if_ether.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <net/if.h>
 
 char LICENSE[] SEC("license") = "Dual MIT/GPL";
 
 struct {
-    __uint(type, BPF_MAP_TYPE_ARRAY);
-    __uint(max_entries, 1);
-    __type(key, u32);
-    __type(value, u32);
+	__uint(type, BPF_MAP_TYPE_ARRAY);
+	__type(key, __u32);
+	__type(value, __u32);
+	__uint(max_entries, 1);
 } intf_count SEC(".maps");
+
+struct {
+	__uint(type, BPF_MAP_TYPE_ARRAY);
+	__type(key, __u32);
+	__type(value, __u32);
+	__uint(max_entries, 1);
+} debug_map SEC(".maps");
 
 struct DemoHeader {
     uint8_t port; // egress port
@@ -21,46 +30,61 @@ struct Headers {
     struct DemoHeader demo;
 };
 
-static inline u16 ntohs(const u16 net) {
-    u8 data[2] = {};
-    __builtin_memcpy(data, &net, sizeof(data));
-    return ((u16)data[1] << 0) | ((u16)data[0] << 8);
-}
-
 SEC("xdp")
-int xdp_redirect(struct xdp_md *ctx) {
-    void *data = NULL, *data_end = NULL;
-    BPF_CORE_READ_INTO(&data, ctx, data);
-    BPF_CORE_READ_INTO(&data_end, ctx, data_end);
-
-    // The received packet frame is too short.
-    if (data + sizeof(struct Headers) > data_end) {
+int xdp_redirect(struct xdp_md *ctx)
+{
+	__u32 key = 0;
+	__u32 debug_val=0;
+	
+    void *data_end = (void *)(long)ctx->data_end;
+    void *data = (void *)(long)ctx->data;
+    ssize_t len = data_end - data;
+    struct Headers* hdrs;
+    
+    bpf_map_update_elem(&debug_map, &key, &debug_val, 0);
+    
+    // Data too short
+    if (data + sizeof(*hdrs) > data_end)
+    {
+    	debug_val=1;
+   		bpf_map_update_elem(&debug_map, &key, &debug_val, 0);
+    	return XDP_DROP;
+   	}
+    
+    hdrs=(struct Headers*)data;
+    uint16_t ethertype = ntohs(hdrs->eth.h_proto);
+    
+    // Unexpected ethernet protocol
+    if (ethertype != 0xdead)
+   	{
+   		debug_val=2;
+   		bpf_map_update_elem(&debug_map, &key, &debug_val, 0);
         return XDP_DROP;
     }
-
-    struct Headers *hdrs = data;
-    struct ethhdr eth;
-    struct DemoHeader demo;
-    BPF_CORE_READ_INTO(&eth, hdrs, eth);
-    BPF_CORE_READ_INTO(&demo, hdrs, demo);
-
-    // Unexpected ethertype
-    uint16_t ethertype = ntohs(eth.h_proto);
-    if (ethertype != 0xdead) {
+        
+    // Count the number of interfaces
+    
+    
+    __u32* value = bpf_map_lookup_elem(&intf_count, &key);
+    if (!value)
+    {
+    	debug_val=3;
+    	bpf_map_update_elem(&debug_map, &key, &debug_val, 0);
         return XDP_DROP;
     }
-
-    // Get the number of interfaces
-    u32 key = 0;
-    u32 *num_intfs = (u32 *)bpf_map_lookup_elem(&intf_count, &key);
-    if (!num_intfs) {
-        return XDP_DROP;
-    }
+    
+    // NULL-check the number of interfaces
+   	
+    __u32 intf_num = *value;
 
     // Destination port out of range
-    if (demo.port >= *num_intfs) {
-        return XDP_DROP;
-    }
-
-    return bpf_redirect(demo.port, 0);
+    if (hdrs->demo.port >= intf_num)
+    {
+    	debug_val=4;
+    	bpf_map_update_elem(&debug_map, &key, &debug_val, 0);
+    	return XDP_DROP;
+	}
+	debug_val=5;
+    bpf_map_update_elem(&debug_map, &key, &debug_val, 0);
+    return bpf_redirect(hdrs->demo.port, 0);
 }
