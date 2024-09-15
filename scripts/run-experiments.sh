cee9001a771033e186caa9f11a08013293825723
kyechou <kuanyenchou@gmail.com>
2024-09-14 23:33:07 -0500

    Update experiment run script

--- scripts/run-experiments.sh
+++ scripts/run-experiments.sh
@@ -3,7 +3,7 @@
 # Run Mimesis experiments.
 #
 
-set -euo pipefail
+set -xeuo pipefail
 
 msg() {
     echo -e "[+] ${1-}" >&2
@@ -17,40 +17,58 @@ die() {
 [ $UID -eq 0 ] && die 'Please run this script without root privilege'
 
 main() {
-    # # Parse script arguments
-    # parse_args "$@"
-
     SCRIPT_DIR="$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
     PROJECT_DIR="$(dirname "${SCRIPT_DIR}")"
     BUILD_DIR="$PROJECT_DIR/build"
     TARGETS_DIR="$BUILD_DIR/targets"
-    # OUTPUT_DIR="$PROJECT_DIR/output"
 
     local target_programs=(
         user-demo-stateless
         user-demo-stateful
-        user-l2-echo
-        user-l2-forward
-        user-ip-echo
         user-ip-stateless
         user-ip-stateful
+        user-ip-echo
+        user-l2-echo
+        user-l2-forward
         kernel-demo-stateless
         kernel-demo-stateful
-        kernel-demo-stateful-d3
+        # kernel-demo-stateful-d3
         kernel-ip-stateless
         kernel-ip-stateful
-        kernel-demo-stateless-pcpp
-        kernel-demo-stateful-pcpp
-        kernel-ip-stateless-pcpp
-        kernel-ip-stateful-pcpp
+        # ebpf-demo-stateless
+        # ebpf-demo-stateful
+        # ebpf-ip-stateless
+        # ebpf-ip-stateful
     )
 
     for program in "${target_programs[@]}"; do
-        for max_depth in {1..2}; do
-            "$SCRIPT_DIR/s2e.sh" -d "$max_depth" -n "$TARGETS_DIR/$program"
-            "$SCRIPT_DIR/s2e.sh" -r
+        for depth in 1 2; do
+            for kfork in 0 1; do
+                for ksymaddr in 0 1; do
+                    local new_project_args=(
+                        -n
+                        -d "$depth"
+                        -t 1800 # timeout: 30 min
+                    )
+                    if [[ "$kfork" -eq 1 ]]; then
+                        new_project_args+=(-kf)
+                    fi
+                    if [[ "$ksymaddr" -eq 1 ]]; then
+                        new_project_args+=(-ks)
+                    fi
+                    new_project_args+=("$TARGETS_DIR/$program")
+
+                    msg "Creating new project.    ------- $(date) -------"
+                    "$SCRIPT_DIR/s2e.sh" "${new_project_args[@]}"
+                    msg "Start model extraction.  ------- $(date) -------"
+                    "$SCRIPT_DIR/s2e.sh" -c -r
+                    msg "Finish model extraction. ------- $(date) -------"
+                done
+            done
         done
     done
+
+    msg "Done!"
 }
 
 main "$@"
