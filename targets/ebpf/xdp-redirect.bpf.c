f67568d0006dd241b0226d8114495ba045d85178
al0wner <sjeon12@illinois.edu>
2024-09-15 14:36:28 -0500

    xdp program update

--- targets/ebpf/xdp-redirect.bpf.c
+++ targets/ebpf/xdp-redirect.bpf.c
@@ -1,9 +1,9 @@
 #include "vmlinux.h"
-
-#include <bpf/bpf_core_read.h>
 #include <bpf/bpf_helpers.h>
+#include <bpf/bpf_endian.h>
 
 char LICENSE[] SEC("license") = "Dual MIT/GPL";
+
 
 struct {
     __uint(type, BPF_MAP_TYPE_ARRAY);
@@ -11,6 +11,20 @@ struct {
     __type(key, u32);
     __type(value, u32);
 } intf_count SEC(".maps");
+
+struct {
+	__uint(type, BPF_MAP_TYPE_ARRAY);
+	__type(key, __u32);
+	__type(value, __u32);
+	__uint(max_entries, 1);
+} debug_map SEC(".maps");
+
+struct {
+	__uint(type, BPF_MAP_TYPE_ARRAY);
+	__type(key, __u32);
+	__type(value, __u64);
+	__uint(max_entries, 1);
+} data_map SEC(".maps");
 
 struct DemoHeader {
     uint8_t port; // egress port
@@ -20,47 +34,61 @@ struct Headers {
     struct ethhdr eth;
     struct DemoHeader demo;
 };
-
+/*
 static inline u16 ntohs(const u16 net) {
     u8 data[2] = {};
     __builtin_memcpy(data, &net, sizeof(data));
     return ((u16)data[1] << 0) | ((u16)data[0] << 8);
 }
-
+*/
 SEC("xdp")
 int xdp_redirect(struct xdp_md *ctx) {
-    void *data = NULL, *data_end = NULL;
-    BPF_CORE_READ_INTO(&data, ctx, data);
-    BPF_CORE_READ_INTO(&data_end, ctx, data_end);
+	__u32 key = 0;
+	__u32 debug_val=0;
+	
+	void *data = (void *)(long)ctx->data;
+	void *data_end = (void *)(long)ctx->data_end;
+	__u64 data_raw = (__u64)data;
+	
+	
 
     // The received packet frame is too short.
     if (data + sizeof(struct Headers) > data_end) {
+    	debug_val=1;
+   		bpf_map_update_elem(&debug_map, &key, &debug_val, 0);
         return XDP_DROP;
     }
 
     struct Headers *hdrs = data;
-    struct ethhdr eth;
-    struct DemoHeader demo;
-    BPF_CORE_READ_INTO(&eth, hdrs, eth);
-    BPF_CORE_READ_INTO(&demo, hdrs, demo);
+    struct ethhdr *eth = &hdrs->eth;
+	struct DemoHeader *demo = &hdrs->demo;
+	bpf_map_update_elem(&debug_map, &key, &debug_val, 0);
+	bpf_map_update_elem(&data_map, &key, &data_raw, 0);
 
     // Unexpected ethertype
-    uint16_t ethertype = ntohs(eth.h_proto);
+    uint16_t ethertype = bpf_ntohs(eth->h_proto);
     if (ethertype != 0xdead) {
+    	debug_val=2;
+   		bpf_map_update_elem(&debug_map, &key, &debug_val, 0);
         return XDP_DROP;
     }
 
     // Get the number of interfaces
-    u32 key = 0;
-    u32 *num_intfs = (u32 *)bpf_map_lookup_elem(&intf_count, &key);
+    u32 *num_intfs = bpf_map_lookup_elem(&intf_count, &key);
     if (!num_intfs) {
+    	debug_val=3;
+    	bpf_map_update_elem(&debug_map, &key, &debug_val, 0);
         return XDP_DROP;
     }
 
     // Destination port out of range
-    if (demo.port >= *num_intfs) {
+    if (demo->port >= *num_intfs) {
+    	debug_val=4;
+    	bpf_map_update_elem(&debug_map, &key, &debug_val, 0);
         return XDP_DROP;
     }
-
-    return bpf_redirect(demo.port, 0);
+    
+    debug_val=5;
+    bpf_map_update_elem(&debug_map, &key, &debug_val, 0);
+    return bpf_redirect(hdrs->demo.port, 0);
 }
--- targets/ebpf/xdp-redirect.c
+++ targets/ebpf/xdp-redirect.c
@@ -1,83 +1,113 @@
-#include <net/if.h>
+// SPDX-License-Identifier: (LGPL-2.1 OR BSD-2-Clause)
+/* Copyright (c) 2020 Facebook */
 #include <stdio.h>
 #include <unistd.h>
-
-#include <bpf/bpf.h>
+#include <sys/resource.h>
 #include <bpf/libbpf.h>
-
-#include "xdp-redirect.skel.h"
-
-static int libbpf_print_fn(enum libbpf_print_level level,
-                           const char *format,
-                           va_list args) {
-    if (level >= LIBBPF_DEBUG) {
-        return 0;
-    }
-    return vfprintf(stderr, format, args);
+#include "xdp_redirect.skel.h"
+#include <net/if.h>
+static int libbpf_print_fn(enum libbpf_print_level level, const char *format, va_list args)
+{
+	return vfprintf(stderr, format, args);
 }
 
-int main() {
-    struct xdp_redirect_bpf *skel;
-    int err;
 
-    /* Set up libbpf errors and debug info callback */
-    libbpf_set_print(libbpf_print_fn);
-    __u32 key = 0;
-    __u32 if_count = 0;
 
-    struct if_nameindex *if_nidxs, *intf;
+int main(int argc, char **argv)
+{
+	struct xdp_redirect_bpf *skel;
+	int err;
+	
+	
+	/* Set up libbpf errors and debug info callback */
+	libbpf_set_print(libbpf_print_fn);
+	__u32 key=0;
+	__u32 if_count=0;
+	__u32 debug_val;
+	__u64 data;
+	
+	struct if_nameindex *if_nidxs, *intf;
+	
+	/* Open BPF application */
+	skel = xdp_redirect_bpf__open();
+	if (!skel) {
+		fprintf(stderr, "Failed to open BPF skeleton\n");
+		return 1;
+	}
+	fprintf(stderr, "Opened BPF skeleton\n");
+	
+	/* Load & verify BPF programs */
+	err = xdp_redirect_bpf__load(skel);
+	if (err) {
+		fprintf(stderr, "Failed to load and verify BPF skeleton\n");
+		goto cleanup;
+	}
+	fprintf(stderr, "Loaded and verified BPF skeleton\n");
+	
+	if_nidxs = if_nameindex();
+    if ( if_nidxs != NULL )
+    {
+        for (intf = if_nidxs; intf->if_index != 0 || intf->if_name != NULL; intf++)
+        {
+        	if_count++;
+        }
 
-    /* Open BPF application */
-    skel = xdp_redirect_bpf__open();
-    if (!skel) {
-        fprintf(stderr, "Failed to open BPF skeleton\n");
-        return 1;
-    }
-    fprintf(stderr, "Opened BPF skeleton\n");
+		err = bpf_map__update_elem(skel->maps.intf_count, &key, 4, &if_count, 4, 0);
+		if (err) {
+			fprintf(stderr, "Failed to update BPF map\n");
+			goto cleanup;
+		}
+		fprintf(stderr, "Updated BPF map\n");
+		
+		/* Attach to every interface */
 
-    /* Load & verify BPF programs */
-    err = xdp_redirect_bpf__load(skel);
-    if (err) {
-        fprintf(stderr, "Failed to load and verify BPF skeleton\n");
-        goto cleanup;
-    }
-    fprintf(stderr, "Loaded and verified BPF skeleton\n");
-
-    err = bpf_map__update_elem(skel->maps.intf_count, &key, 4, &if_count, 4, 0);
-    if (err) {
-        fprintf(stderr, "Failed to update BPF map\n");
-        goto cleanup;
-    }
-    fprintf(stderr, "Updated BPF map\n");
-
-    /* Attach to every interface */
-    if_nidxs = if_nameindex();
-    if (if_nidxs != NULL) {
-        for (intf = if_nidxs; intf->if_index != 0 || intf->if_name != NULL;
-             intf++) {
-            bpf_program__attach_xdp(skel->progs.xdp_redirect, intf->if_index);
+        for (intf = if_nidxs; intf->if_index != 0 || intf->if_name != NULL; intf++)
+        {
+        	bpf_program__attach_xdp(skel->progs.xdp_redirect, intf->if_index);
         }
         if_freenameindex(if_nidxs);
     }
+	
+	/*
+	err = xdp_redirect_bpf__attach(skel);
+	if (err) {
+		fprintf(stderr, "Failed to attach BPF skeleton\n");
+		goto cleanup;
+	}
+	*/
+	printf("Successfully started! Please run `sudo cat /sys/kernel/debug/tracing/trace_pipe` "
+	       "to see output of the BPF programs.\n");
 
-    /*
-    err = xdp_redirect_bpf__attach(skel);
-    if (err) {
-        fprintf(stderr, "Failed to attach BPF skeleton\n");
-        goto cleanup;
-    }
-    */
-    printf("Successfully started! Please run `sudo cat "
-           "/sys/kernel/debug/tracing/trace_pipe` "
-           "to see output of the BPF programs.\n");
-
-    for (;;) {
-        /* trigger our BPF program */
-        fprintf(stderr, ".");
-        sleep(1);
-    }
+	for (;;) {
+		/* trigger our BPF program */
+		
+		err = bpf_map__lookup_elem(skel->maps.debug_map, &key, sizeof(key), &debug_val, sizeof(debug_val), 0);
+		switch(debug_val) {
+			case 0:
+				fprintf(stderr, "No packets processed yet\n");
+				break;
+			case 1:
+				fprintf(stderr, "Data too short\n");
+				break;
+			case 2:
+				fprintf(stderr, "Unexpected ethernet protocol\n");
+				break;
+			case 3:
+				fprintf(stderr, "Count the number of interfaces\n");
+				break;
+			case 4:
+				fprintf(stderr, "Destination port out of range\n");
+				break;
+			case 5:
+				err = bpf_map__lookup_elem(skel->maps.data_map, &key, sizeof(key), &data, sizeof(data), 0);
+				fprintf(stderr, "data=%016llx\n", data);
+				fprintf(stderr, "Redirected\n");
+				break;
+		}
+		sleep(1);
+	}
 
 cleanup:
-    xdp_redirect_bpf__destroy(skel);
-    return -err;
+	xdp_redirect_bpf__destroy(skel);
+	return -err;
 }
